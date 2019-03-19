098b57e39119fa8b06c3cfa50662e96ca82dab14
Nikita Koshelev <koshelev.nd@gmail.com>
2019-03-19 23:53:02 +0200

    Add user and subscription into DB

---
+++ settings.py
@@ -0,0 +1,4 @@
+PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
+    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+
+API = "725325011:AAEH9KeN78EkFkFDBunTR8BbWWNOeoPqQbQ"
